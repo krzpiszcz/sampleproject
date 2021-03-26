92e2482f82e090564472733aeefa2d97bbea9c6c
krzpiszcz <kpiszcz@yahoo.com>
2021-03-26 14:52:12 +0100

    changes done

--- Testpyt.py
+++ Testpyt.py
@@ -1,1 +1,23 @@
-print("testing pycharm")
+# Python program to illustrate  
+# *args for variable number of arguments
+def myFun(*argv):
+    for arg in argv:
+        print(arg)
+
+
+myFun('Hello', 'Welcome', 'to', 'GeeksforGeeks')
+
+def myfun2(**kkwargs):
+    #for arg in argk:
+        print("kwargs: ", kkwargs)
+
+myfun2( jeden='Welcome', drugi="chyba", trzeci="taknie" )
+
+def fib(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    while a < n:
+        print(a, a+1, sep=' ', end=' ')
+        a, b = b, a+b
+    print()
+
+fib(4)
